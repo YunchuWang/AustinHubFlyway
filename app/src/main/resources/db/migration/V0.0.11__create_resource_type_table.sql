428aa9bb6c428f008e4a083fbc241e356b4b2013
ssdd2013 <shutao2011@gmail.com>
2021-05-31 19:31:00 -0500

    SQ-01: insert intial values in db migration files

--- app/src/main/resources/db/migration/V0.0.11__create_resource_type_table.sql
+++ app/src/main/resources/db/migration/V0.0.11__create_resource_type_table.sql
@@ -7,3 +7,5 @@ CREATE TABLE `resource_type` (
   `yearlyPrice` double(5,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `resource_type` VALUES (1,'ads','Ads lets you promote your business at home page with flexible customizations',34.99,99.99,399.99),(2,'booth','A resource to display your business information in clean text view',19.99,54.99,209.99),(3,'job','A hiring post to attract talents for your business',9.99,26.99,99.99);
