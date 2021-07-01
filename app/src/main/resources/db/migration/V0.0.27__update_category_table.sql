b87d7cce9d6cb5c5bca51dbb9d2eeb0d2f166941
peterstone2017 <yunchuwang5@gmail.com>
2021-06-30 23:26:03 -0500

    [BW]: update category names

---
+++ app/src/main/resources/db/migration/V0.0.27__update_category_table.sql
@@ -0,0 +1,5 @@
+UPDATE `category` SET `name` = 'mybooths' WHERE (`id` = '8');
+UPDATE `category` SET `name` = 'accountinfo', `link` = 'accountinfo' WHERE (`id` = '7');
+UPDATE `category` SET `name` = 'myjobs' WHERE (`id` = '9');
+UPDATE `category` SET `name` = 'myads' WHERE (`id` = '10');
+UPDATE `category` SET `name` = 'orderhistory' WHERE (`id` = '11');
